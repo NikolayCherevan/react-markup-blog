cc88ba7b24ea0b5005fcd932c20f9767072416d9
krivtsuna <krivtsuna@gmail.com>
2021-07-20 02:30:57 +0300

    bugfix issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import {Layout, Menu} from 'antd';
-import {BrowserRouter as Router, Switch, Route, Link, useParams} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link, useLocation} from "react-router-dom";
 import {HashRouter} from "react-router-dom";
-import {DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined} from '@ant-design/icons';
-import {createBrowserHistory} from "history";
-import {LinkContainer} from 'react-router-bootstrap';
+import {DesktopOutlined, PieChartOutlined, Html5Outlined} from '@ant-design/icons';
 const {Header, Content, Footer, Sider} = Layout;
-const {SubMenu} = Menu;
 
-const history = createBrowserHistory();
-function Home() {
+function usePageViews() {
+    let location = useLocation();
+    return location.pathname
 
+}
+function NotFoundPage() {
     return (
         <div>
-            hom
+            oh that page not found sorry
+        </div>
+    );
+}
+function IntroHTML() {
+    return (
+        <div>
+            Введение в HTML
         </div>
     );
 }
 function CSS() {
-
     return (
         <div>
             css
@@ -35,69 +41,138 @@ function MoreCSS() {
         </div>
     );
 }
-class SiderDemo extends React.Component {
-    state = {
-        collapsed: false
-    };
+function HTML() {
+    return (
+        <div>
+            HTML
+        </div>
+    );
+}
+function MoreHTML() {
 
-    onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({collapsed});
-    };
+    return (
+        <div>
+            MoreHTML
+        </div>
+    );
+}
+function RenderMenuHTML() {
+    return (
+        <div>
+            <Menu theme="dark">
+                <Menu.Item
+                    className={`${usePageViews() == '/introHTML'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="1"
+                    icon={< Html5Outlined />}>
+                    <Link to='/introHTML'>Введение</Link>
+                </Menu.Item>
+                <Menu.Item
+                    className={`${usePageViews() == '/html'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="2"
+                    icon={< Html5Outlined />}>
+                    <Link to='/html'>htmlFirst</Link>
+                </Menu.Item>
+                <Menu.Item
+                    className={`${usePageViews() == '/moreHTML'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="3"
+                    icon={< Html5Outlined />}>
+                    <Link to='/moreHTML'>htmlSecond</Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    )
+}
+function RenderMenuCSS() {
+    return (
+        <div>
+            <Menu theme="dark">
+                <Menu.Item
+                    className={`${usePageViews() == '/introCSS'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="1"
+                    icon={< Html5Outlined />}>
+                    <Link to='/introCSS'>Введение</Link>
+                </Menu.Item>
+                <Menu.Item
+                    className={`${usePageViews() == '/css'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="2"
+                    icon={< Html5Outlined />}>
+                    <Link to='/css'>cssFirst</Link>
+                </Menu.Item>
+                <Menu.Item
+                    className={`${usePageViews() == '/morecss'
+                    ? 'ant-menu-item-selected'
+                    : ''}`}
+                    key="3"
+                    icon={< Html5Outlined />}>
+                    <Link to='/morecss'>cssSecond</Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    )
+}
+function SiderDemo() {
+    return (
+        <Router>
+            <Layout style={{
+                minHeight: '100vh'
+            }}>
+                <Sider collapsible>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to='/htmlMain'>HTML</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to='/cssMain'>CSS</Link>
+                        </Menu.Item>
 
-    render() {
-        const {collapsed} = this.state;
-        return (
-<HashRouter
-   basename="/react-markup-blog"
->
-            <Router>
-                <Layout style={{
-                    minHeight: '100vh'
-                }}>
-                    <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-                        <div className="logo"/>
-                        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-                            <Menu.Item key="1" icon={< PieChartOutlined />}>
-                                <Link to="/react-markup-blog">Home</Link>
-                            </Menu.Item>
-                            <Menu.Item key="2" icon={< DesktopOutlined />}>
-                                <Link to="/css">CSS</Link>
-                            </Menu.Item>
+                    </Menu>
+                        <HashRouter>< RenderMenuHTML />
+                            <Switch>
+                                <Route exact path='/htmlMain' child={< RenderMenuHTML />}/>
+                                <Route exact path='/cssMain' child={< RenderMenuCSS />}/>
+                            </Switch>
+                        </HashRouter>
 
-                            <Menu.Item key="3" icon={< DesktopOutlined />}>
-                                <Link to="/morecss">More css</Link>
-                            </Menu.Item>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header
+                        className="site-layout-background"
+                        style={{
+                        padding: 0
+                    }}/>
 
-                        </Menu>
-                    </Sider>
-                    <Layout className="site-layout">
-                        <Header
-                            className="site-layout-background"
-                            style={{
-                            padding: 0
-                        }}/>
-
-                        <Content
-                            style={{
-                            margin: '0 16px'
-                        }}>
-                                <Switch>
-                                    <Route exact path="/"  children={< Home />}/>
-                                    <Route path="/css" children={< CSS />}/>
-                                    <Route path="/morecss" children={< MoreCSS />}/>
-                                </Switch>
-                        </Content>
-                        <Footer
-                            style={{
-                            textAlign: 'center'
-                        }}>
-                            dev blog</Footer>
-                    </Layout>
+                    <Content
+                        style={{
+                        margin: '0 16px'
+                    }}>
+                        <HashRouter>
+                            <Switch>
+                          <Route exact path='/introHTML' render={props => <IntroHTML {...props}/>}/>
+                                <Route exact path='/html' render={props => <HTML {...props}/>}/>
+                                <Route path='/moreHTML' render={props => <MoreHTML {...props}/>}/>
+                                <Route render={props => <NotFoundPage {...props}/>}/>
+                            </Switch>
+                        </HashRouter>
+                    </Content>
+                    <Footer
+                        style={{
+                        textAlign: 'center'
+                    }}>
+                        dev blog</Footer>
                 </Layout>
-            </Router>
-</HashRouter>
-        );
-    }
+            </Layout>
+        </Router>
+    );
 }
 export default SiderDemo;
