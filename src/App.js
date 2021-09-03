6ad27ac7e6b25b5c032c522cb014698cc35d5a82
Nikolay <cherevan.n.s@gmail.com>
2021-09-03 12:43:09 +0300

    Add code tag package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/Home.jsx';
 import bootstrap from 'bootstrap';
+
 import {
     HashRouter
 } from "react-router-dom";
--- src/pages/cssArticles/subcategoriesCSS/CSS6.jsx
+++ src/pages/cssArticles/subcategoriesCSS/CSS6.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import banner from './imagesCSS6/banner.jpg';
-import './stylesCSS6/_stylesCSS6.scss'
+import './stylesCSS6/_stylesCSS6.scss';
+import SyntaxHighLighter from 'react-syntax-highlighter';
 class CSS6 extends Component {
     render() {
         return (
@@ -13,6 +14,22 @@ class CSS6 extends Component {
                         Зачем, а главное - для чего?
                     </h2>
                     <div> Основная идея <span className="border border-1 p-1">flex</span>  заключается в том, чтобы дать контейнеру возможность изменять ширину / высоту (и порядок) своих элементов, чтобы наилучшим образом заполнить доступное пространство (в основном для размещения всех типов устройств отображения и размеров экрана). <span className="border border-1 p-1">flex</span> расширяет элементы, чтобы заполнить доступное свободное пространство, или сжимает их, чтобы предотвратить переполнение.</div>
+                    </div>
+                   
+                </section>
+                <section>
+                    <div className="container">
+                    <h2 className="text-center mt-3">
+                        Fexbox свойства
+                    </h2>
+                    <div className="code">
+                    <SyntaxHighLighter language="css">
+{`.container { 
+display: flex; /* or inline-flex */
+      }`}
+</SyntaxHighLighter>
+                        </div>
+                  
                     </div>
                    
                 </section>
